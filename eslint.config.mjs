b85c8947a8a0d780d288dabac594136bca7ab4b2
Muhammad Adjie Bimaditya <adbimaditya@gmail.com>
2025-03-26 21:17:31 +0700

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  {
+    ignores: ['node_modules', 'dist', 'eslint.config.mjs'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.js'],
+        },
+      },
+    },
+  },
+  ...compat.extends('airbnb-base'),
+  ...compat.extends('prettier'),
+];
